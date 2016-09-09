e01fddb42a6801a3e5a1369f68f7522ee09d6719
ian Wilcox <ian Wilcox>
2016-09-09 10:29:10 -0700

    first commit

---
+++ Modules/HomeModules.cs
@@ -0,0 +1,13 @@
+using Nancy;
+using AddressBook.Objects;
+
+namespace AddressBook
+{
+  public class HomeModule : NancyModule
+  {
+    public HomeModule()
+    {
+      Get["/"] = _ => HOME PAGE
+    }
+  }
+}
